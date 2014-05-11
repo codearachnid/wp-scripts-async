a0124201c73262fae016a707c3a43aa8fbe9b1c2
Timothy Wood <tim@imaginesimplicity.com>
2014-05-11 11:33:57 -0400

    initial plugin file

---
+++ wp-frontend-enqueue-script.php
@@ -0,0 +1,19 @@
+<?php
+/*
+
+Plugin Name: wp_require_enqueue_script
+Plugin URI:
+Description: Extending wp_enqueue_script to be compatible with RequireJS
+Version: 1.5
+Author: Timothy Wood (@codearachnid)
+Author URI: http://codearachnid.com
+Text Domain: wp_require_enqueue_script
+
+*/
+
+namespace WP_Frontend_Enqueue;
+
+
+class WP_Frontend_Enqueue {
+
+} 
